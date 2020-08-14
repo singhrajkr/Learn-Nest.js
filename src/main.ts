3705be99e139d41eb5d12820c99a415593212b51
Raj Kumar <raj.kumar.jsdeveloper@gmail.com>
2020-08-15 02:22:08 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Validation Pipe whitelist: true removes unknown property from request payload
   // Validation Pipe forbidNonWhitelisted: true stops processing request when unknown property in passed in request payload
-  // Validation Pipe transform: true transform request payload to DTO instance and even it will transform the query parameter to defined type from string(all request comes from network are string bedefault)
+  // Validation Pipe transform: true transform request payload to DTO instance and even it will transform the query parameter to defined type from string(all request comes from network are string by default)
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true,
